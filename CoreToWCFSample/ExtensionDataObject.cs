f5a1a149e980ea39f066d1c5024aa19a712877a3
Gary Schmidt <gschmidt@oakwoodsys.com>
2018-08-24 09:51:29 -0500

    Add project files.

---
+++ CoreToWCFSample/ExtensionDataObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.Runtime.Serialization
+{
+	public class ExtensionDataObject
+	{
+	}
+
+	internal interface IExtensibleDataObject
+	{
+	}
+}
---
+++ CoreToWCFSample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+
+namespace CoreToWCFSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Here we go...");
+
+			Binding binding = null;
+			binding = new BasicHttpBinding(BasicHttpSecurityMode.None);
+
+
+			var factory = new ChannelFactory<IService1>(binding, new EndpointAddress("http://localhost:8733/Design_Time_Addresses/WCFSample/Service1"));
+
+			var serviceProxy = factory.CreateChannel();
+
+			var result = serviceProxy.GetData(5);
+
+			Console.WriteLine(result.ToString());
+
+			var test = Console.ReadLine();
+        }
+    }
+}
